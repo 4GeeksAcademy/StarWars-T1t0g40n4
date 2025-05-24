533af880997911a8ac7044fae12d92107df7f361
T1t0-G40n4 <titogaona84@gmail.com>
2025-05-24 15:55:43 +0000

    Starwars T1t0-G40n4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
+
+	useEffect(() => {
+
+		const get_people = async () => {
+			const loadPeopleData = await fetch('https://www.swapi.tech/api/people/', {
+				method:'GET'
+			})
+			const responsePeople = await loadPeopleData.json();
+			dispatch ({
+				type:'set-people',
+				payload: responsePeople
+			})
+		}
+		const responsePeople = asyn () =>{
+			
+		}
+
+	}, [])
+
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+
 		</div>
 	);
 }; 
