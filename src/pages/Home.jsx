251a69ec576c46f3eb18bfb6a78d3e9d916ffbf4
T1t0-G40n4 <titogaona84@gmail.com>
2025-05-28 19:50:37 +0000

    Aca va el proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { People } from "./People.jsx";
 
 export const Home = () => {
 
@@ -10,46 +10,64 @@ export const Home = () => {
 
 		const get_people = async () => {
 			const loadPeopleData = await fetch('https://www.swapi.tech/api/people/', {
-				method:'GET'
+				method: 'GET'
 			})
 			const reponsePeople = await loadPeopleData.json();
-			dispatch ({
-				type:'set-people',
+			dispatch({
+				type: 'set-people',
 				payload: {
-					people : reponsePeople
+					people: reponsePeople.results
 				}
 			})
+
 		}
-		const get_planets = async () =>{
-			const loadPlanetsData = await fetch ('https://www.swapi.tech/api/planets/',{
-				method:'GET'
+		const get_planets = async () => {
+			const loadPlanetsData = await fetch('https://www.swapi.tech/api/planets/', {
+				method: 'GET'
 			})
 			const reponsePlanet = await loadPlanetsData.json();
 			dispatch({
-				type:'set-planets',
-				payload : reponsePlanet
+				type: 'set-planets',
+				payload: {
+					planets : reponsePlanet.results
+				}
 			})
-		const get_starShips = async () =>{
-			const loadShipsData = await fetch ('https://www.swapi.tech/api/starships/',{
-				method:'GET'
+		};
+		const get_starShips = async () => {
+			const loadShipsData = await fetch('https://www.swapi.tech/api/starships/', {
+				method: 'GET'
 			})
 			const reponseShips = await loadShipsData.json();
 			dispatch({
-				type:'set-starships',
-				payload : reponseShips
+				type: 'set-starships',
+				payload: {
+					starships: reponseShips.results
+				}
 			})
-		}
+		};
 
 		get_people();
 		get_planets();
 		get_starShips();
+		
+		
 	}, [])
 
-
+		console.log(store.people);
+		console.log(store.planets);
+		console.log(store.starships);
+		
 	return (
 		<div className="text-center mt-5">
-
-
+			{Array.isArray(store.people) && store.people.length>0?(
+				store.people.map(person => (
+					<div className="" key={person.uid}>
+						<People person={person}/>
+					</div>
+				))
+			) : (
+				null
+			)}
 		</div>
 	);
 }; 
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,7 +1,15 @@
-export const People = () =>{
+export const People = ({person}) => {
     return (
-         <div className="container">
-
+        <div className="container">
+            <div className="card" style={{maxWidth:"18rem" }}>
+                <img src={`https://www.swapi.tech/api/people/${person.uid}`} className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{person.name}</h5>
+                        <p className="card-text">{person.uid}</p>
+                        <p className="card-text">{person.gender}</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
         </div>
     )
 }
